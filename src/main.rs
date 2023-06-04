f1f4001453cf16a61bfc2ac5569cf322126b492e
[Cody McKinney] <82977069+codymck@users.noreply.github.com>
2023-06-03 17:43:30 -0700

    buffered reader + output to txt

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,30 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
-fn main() {}
+// Result has 2 variants OK and Err
+// enum Result<T, E> {
+// Ok(T),
+// Err(E), }
+// where T represents the data typeof the value returns
+// and E the type of error
+
+fn main() {
+    let path = "lines.txt";
+
+    let output = File::create(path);
+    let mut output = match output {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Problem creating file : {:?}", error);
+        }
+    };
+
+    write!(output, "Just some\nRandom words").expect("Failed to write to file");
+
+    let input = File::open(path).unwrap();
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        println!("{}", line.unwrap());
+    }
+}
